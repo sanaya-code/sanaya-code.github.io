81afa4c138bfbc2c34a899efac6d241cc8813dc5
parvezk <parvezk>
2025-08-25 15:32:19 +0530

    info websites

--- info/data_files/miriam.js
+++ info/data_files/miriam.js
@@ -1,6 +1,8 @@
 
 
 var miriam = `[
+  { "label": "Quiz Local", "url": "file:///mnt/0cbf398f-1dda-49a3-b3aa-8919127e7eef/repos/sanaya-code.github.io/quiz/index.html" },
+  { "label": "Quiz", "url": "https://sanaya-code.github.io/quiz/index.html" },
   { "label": "test", "url": "https://google.com" }
   ]`;
   
--- info/data_files/sanaya.js
+++ info/data_files/sanaya.js
@@ -5,6 +5,8 @@
     { "label": "Miriam Drive", "url": "https://drive.google.com/drive/folders/1ZycjwMmNaHQpgNn7VKEYx2LDSaiFe5-y?usp=drive_link" },
     {"label": "Github sanaya", "url":"https://github.com/sanaya-code/sanaya-code.github.io/tree/main/quiz/data/school/c1_miriam"}, 
     {"label": "Sanaya Fee", "url":"https://stxbplerp.in/DefaultMIS1.aspx"}, 
+    { "label": "Quiz Local", "url": "file:///mnt/0cbf398f-1dda-49a3-b3aa-8919127e7eef/repos/sanaya-code.github.io/quiz/index.html" },
+    { "label": "Quiz", "url": "https://sanaya-code.github.io/quiz/index.html" },
     {"label": "Test", "url":"test"}
   ]`;
   
