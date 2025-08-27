cc9b4307bdc8975f0afc582997f98fc49ff0de8b
parvezk <parvezk>
2025-08-27 16:14:02 +0530

    info wesite

--- info/data_scripts/miriam.js
+++ info/data_scripts/miriam.js
@@ -3,6 +3,7 @@
 var miriam = `[
   { "label": "Quiz Local", "url": "file:///mnt/0cbf398f-1dda-49a3-b3aa-8919127e7eef/repos/sanaya-code.github.io/quiz/index.html" },
   { "label": "Quiz", "url": "https://sanaya-code.github.io/quiz/index.html" },
+  { "label": "Miriam Drive", "url": "https://drive.google.com/drive/folders/1ZycjwMmNaHQpgNn7VKEYx2LDSaiFe5-y?usp=drive_link" },
   { "label": "test", "url": "https://google.com" }
   ]`;
   
