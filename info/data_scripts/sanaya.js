18c1faeeabac49eb800ca74e69d793d1fc17da8e
parvezk <parvezk>
2025-09-01 10:11:51 +0530

    info page updated

--- info/data_scripts/sanaya.js
+++ info/data_scripts/sanaya.js
@@ -3,9 +3,14 @@
   var sanaya = `[
     { "label": "Sanaya Drive", "url": "https://drive.google.com/drive/folders/1eHGRo-noah74VcrxpVUpIZVQwzn_OrvR?usp=drive_link" },
     {"label": "Github sanaya", "url":"https://github.com/sanaya-code/sanaya-code.github.io/tree/main/quiz/data/school/c1_miriam"}, 
+    {"label": "Github Actions", "url":"https://github.com/sanaya-code/sanaya-code.github.io/actions"},
     {"label": "Sanaya Fee", "url":"https://stxbplerp.in/DefaultMIS1.aspx"}, 
     { "label": "Quiz Local", "url": "file:///mnt/0cbf398f-1dda-49a3-b3aa-8919127e7eef/repos/sanaya-code.github.io/quiz/index.html" },
     { "label": "Quiz", "url": "https://sanaya-code.github.io/quiz/index.html" },
     {"label": "Test", "url":"test"}
   ]`;
   
+  
+
+   
+    
