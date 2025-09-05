d8ddca9902d9c2ac732e609578c408e62ddf4226
parvezk <parvezk>
2025-09-05 12:59:56 +0530

    info website

--- info/data_scripts/others.js
+++ info/data_scripts/others.js
@@ -1,9 +1,9 @@
 // Global variables - no export
 
 var others = `[
-    { "label": "Chat GPT" "url": "https://chatgpt.com/" },
-    { "label": "Perplexity" "url": "https://www.perplexity.ai/" },
-    { "label": "Deepseek" "url": "https://chat.deepseek.com/" },
+    { "label": "Chat GPT", "url": "https://chatgpt.com/" },
+    { "label": "Perplexity", "url": "https://www.perplexity.ai/" },
+    { "label": "Deepseek", "url": "https://chat.deepseek.com/" },
     { "label": "Whatsapp", "url": "https://web.whatsapp.com/" }
   ]`;
   
