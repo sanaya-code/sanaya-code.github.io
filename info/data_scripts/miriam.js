f8db263ceaf0815a3e2553adea78e2902bbfa6cc
parvezk <parvezk>
2025-09-07 22:47:55 +0530

    info website

--- info/data_scripts/miriam.js
+++ info/data_scripts/miriam.js
@@ -3,7 +3,9 @@
 var miriam = `[
   { "label": "Quiz Local", "url": "file:///mnt/0cbf398f-1dda-49a3-b3aa-8919127e7eef/repos/sanaya-code.github.io/quiz/index.html" },
   { "label": "Quiz", "url": "https://sanaya-code.github.io/quiz/index.html" },
+  { "label": "Class 1 drive", "url": "https://drive.google.com/drive/u/4/folders/1_AuwOi6CcabXFo-hOm6mZvdz-wa-U0mw" },
   { "label": "Miriam Drive", "url": "https://drive.google.com/drive/folders/1ZycjwMmNaHQpgNn7VKEYx2LDSaiFe5-y?usp=drive_link" },
-  { "label": "test", "url": "https://google.com" }
+  { "label": "info", "url": "https://docs.google.com/spreadsheets/d/1SXTgTBl8ls8qrfvpleTosNAxNx944R5rZsmEsaC89Z8/edit?gid=0#gid=0" },
+  { "label": "fee", "url": "https://sttheresaerp.com/DefaultMIS1.aspx" }
   ]`;
   
