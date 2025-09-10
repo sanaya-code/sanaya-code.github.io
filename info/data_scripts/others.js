8b9d318737e27eb5cbd09869cd56d0b596a01e15
parvezk <parvezk>
2025-09-10 16:22:40 +0530

    info web page

--- info/data_scripts/others.js
+++ info/data_scripts/others.js
@@ -1,10 +1,14 @@
 // Global variables - no export
 
 var others = `[
+    { "label": "Gmail", "url": "https://mail.google.com" },
+    { "label": "Whatsapp", "url": "https://web.whatsapp.com/" },
+    { "label": "Telegram", "url": "https://web.telegram.org/" },
+    { "label": "Youtube", "url": "https://www.youtube.com/" },
+    { "label": "Amazon", "url": "https://www.amazon.in/" },
     { "label": "Chat GPT", "url": "https://chatgpt.com/" },
     { "label": "Perplexity", "url": "https://www.perplexity.ai/" },
-    { "label": "Deepseek", "url": "https://chat.deepseek.com/" },
-    { "label": "Whatsapp", "url": "https://web.whatsapp.com/" }
+    { "label": "Deepseek", "url": "https://chat.deepseek.com/" }
   ]`;
   
 
