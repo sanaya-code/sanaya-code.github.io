84262634a48a26e76e8823e00c88d3dc80fd9f7f
parvezk <parvezk>
2025-10-15 19:46:28 +0530

    info website

--- info/data_scripts/phd.js
+++ info/data_scripts/phd.js
@@ -5,7 +5,7 @@ var phd = `[
     { "label": "Paper_Doc", "url": "https://docs.google.com/document/d/1ZIRn_VPI_FYx2c5YYSt8XfbKbVdPMLM_vYBKUOMWlR8/edit?usp=sharing" },
     { "label": "Didi Share", "url": "https://drive.google.com/drive/folders/1p2IAWyPVpYbCZXndjhbVmb7K_PBwHlQL" },
     { "label": "Science Direct", "url": "https://www.sciencedirect.com/search?qs=LLM+MCQ" },
-    { "label": "IEEE", "url":"https://ieeexplore.ieee.org/Xplore/home.jsp"}
+    { "label": "IEEE", "url":"https://ieeexplore.ieee.org/Xplore/home.jsp"},
     { "label": "Mendeley", "url":"https://www.mendeley.com/search/"},
     { "label": "Chat GPT", "url": "https://chatgpt.com/" },
     { "label": "Perplexity", "url":"https://www.perplexity.ai/"}, 
