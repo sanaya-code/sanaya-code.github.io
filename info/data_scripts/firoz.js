c3bdb0e6c0887b33bcd66e22b365325c34889049
parvezk <parvezk>
2025-11-04 09:41:59 +0530

    info firoz

--- info/data_scripts/firoz.js
+++ info/data_scripts/firoz.js
@@ -3,5 +3,6 @@
 var firoz = `[
   { "label": "Plot", "url": "https://drive.google.com/drive/folders/1d3GP3fH2_U3ZtXt00NFVzwvoGxYWT2TU" },
   {"label": "Family Drive", "url":"https://drive.google.com/drive/folders/1MUTMqBSMJrL2jt3TwSsHQeFs5QExT3p8?usp=drive_link"},
+  {"label": "Ultimatix", "url":"https://www.ultimatix.net"},
   {"label": "Test", "url":"test"}
 ]`;
