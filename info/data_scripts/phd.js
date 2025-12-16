5f1e05cd3d3deca900727b62f7e7358b81cca587
parvezk <parvezk>
2025-12-16 22:46:50 +0530

    res help

--- info/data_scripts/phd.js
+++ info/data_scripts/phd.js
@@ -2,11 +2,11 @@
 
 var phd = `[
     { "label": "PHD", "url": "https://drive.google.com/drive/folders/1ICrY7PwX5_tEBEUe4-5jkXGUc33RN84Q?usp=drive_link" },
-    { "label": "Search Prompts", "url": "https://docs.google.com/document/d/1ZIRn_VPI_FYx2c5YYSt8XfbKbVdPMLM_vYBKUOMWlR8/edit?usp=sharing" },
+    { "label": "Prompts", "url": "https://github.com/parvezk-rdata/study/blob/main/lxdocs/res_help/chapters/chap01/intro.tex" },
     { "label": "Paper lib", "url":"https://drive.google.com/drive/folders/1CHxerhW5LKB-Pp6hTAgdFij9oYYiMJ1T?usp=drive_link"},
-    { "label": "Prom", "url": "https://docs.google.com/document/d/1B2Ij2yVy91JPsJ2CLnrrgkhIRc2BF4u82gBXQ2Wm7J0/edit?usp=drive_link" },
+    { "label": "Latex", "url": "https://github.com/parvezk-code/Library/blob/master/Learning/programming/Latex/help_book/zz_pdf/main.pdf" },
     { "label": "Extracts", "url": "https://docs.google.com/document/d/1A1TxoCcwPWYFVQRJFUn9aIWrFc0TdJhutECsaX5mb68/edit?usp=drive_link" },
-    { "label": "Public Share", "url": "https://drive.google.com/drive/folders/1p2IAWyPVpYbCZXndjhbVmb7K_PBwHlQL?usp=drive_link" },
+    { "label": "Public Share", "url": "https://drive.google.com/drive/folders/1cRgu02cf9HCJNdplP4H40eKb4pB8ViyQ?usp=drive_link" },
     { "label": "Science Direct", "url": "https://www.sciencedirect.com/search?qs=LLM+MCQ" },
     { "label": "IEEE", "url":"https://ieeexplore.ieee.org/Xplore/home.jsp"},
     { "label": "Mendeley", "url":"https://www.mendeley.com/search/"},
