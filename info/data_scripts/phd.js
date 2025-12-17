4993ebf5f539b3dcd70972c9f336b142a71b04ce
parvezk <parvezk>
2025-12-17 21:21:54 +0530

    info phd

--- info/data_scripts/phd.js
+++ info/data_scripts/phd.js
@@ -2,11 +2,13 @@
 
 var phd = `[
     { "label": "PHD", "url": "https://drive.google.com/drive/folders/1ICrY7PwX5_tEBEUe4-5jkXGUc33RN84Q?usp=drive_link" },
-    { "label": "Prompts", "url": "https://github.com/parvezk-rdata/study/blob/main/lxdocs/res_help/chapters/chap01/intro.tex" },
-    { "label": "Paper lib", "url":"https://drive.google.com/drive/folders/1CHxerhW5LKB-Pp6hTAgdFij9oYYiMJ1T?usp=drive_link"},
-    { "label": "Latex", "url": "https://github.com/parvezk-code/Library/blob/master/Learning/programming/Latex/help_book/zz_pdf/main.pdf" },
-    { "label": "Extracts", "url": "https://docs.google.com/document/d/1A1TxoCcwPWYFVQRJFUn9aIWrFc0TdJhutECsaX5mb68/edit?usp=drive_link" },
     { "label": "Public Share", "url": "https://drive.google.com/drive/folders/1cRgu02cf9HCJNdplP4H40eKb4pB8ViyQ?usp=drive_link" },
+    { "label": "Paper lib", "url":"https://drive.google.com/drive/folders/1CHxerhW5LKB-Pp6hTAgdFij9oYYiMJ1T?usp=drive_link"},
+    { "label": "Extracts", "url": "https://docs.google.com/document/d/1A1TxoCcwPWYFVQRJFUn9aIWrFc0TdJhutECsaX5mb68/edit?usp=drive_link" },
+    { "label": "Templates Lax", "url":"https://github.com/parvezk-rdata/study/tree/main/lxdocs/res_help/chapters/chap02_samples"},
+    { "label": "Prompts", "url": "https://github.com/parvezk-rdata/study/blob/main/lxdocs/res_help/chapters/chap01/intro.tex" },
+    { "label": "Git", "url":"https://github.com/parvezk-rdata/study"},
+    { "label": "Latex", "url": "https://github.com/parvezk-code/Library/blob/master/Learning/programming/Latex/help_book/zz_pdf/main.pdf" },
     { "label": "Science Direct", "url": "https://www.sciencedirect.com/search?qs=LLM+MCQ" },
     { "label": "IEEE", "url":"https://ieeexplore.ieee.org/Xplore/home.jsp"},
     { "label": "Mendeley", "url":"https://www.mendeley.com/search/"},
@@ -14,7 +16,6 @@ var phd = `[
     { "label": "Perplexity", "url":"https://www.perplexity.ai/"}, 
     { "label": "Overleaf", "url":"https://www.overleaf.com/project/67ac877294ce4878abe42309"},
     { "label": "scispace", "url":"https://scispace.com"},
-    { "label": "Git", "url":"https://github.com/parvezk-rdata/study"},
     { "label": "Google Colab", "url":"https://colab.research.google.com/"},
     { "label": "Test", "url":"test"}
   ]`;
