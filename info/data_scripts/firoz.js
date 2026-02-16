5294ed39e647e7433bf8aaf7d8428359bc3c88b5
parvezk <parvezk>
2026-02-16 12:12:40 +0530

    info

--- info/data_scripts/firoz.js
+++ info/data_scripts/firoz.js
@@ -4,5 +4,6 @@ var firoz = `[
   { "label": "Plot", "url": "https://drive.google.com/drive/folders/1d3GP3fH2_U3ZtXt00NFVzwvoGxYWT2TU" },
   {"label": "Family Drive", "url":"https://drive.google.com/drive/folders/1MUTMqBSMJrL2jt3TwSsHQeFs5QExT3p8?usp=drive_link"},
   {"label": "Ultimatix", "url":"https://www.ultimatix.net"},
+  {"label": "TMH Login 11F2025/001303", "url":"https://tmc.gov.in/m_webemr2/emr/login"},
   {"label": "Test", "url":"test"}
 ]`;
