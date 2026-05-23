522fa0fe236a43fe6af7a906c4db7091a7d2b26d
parvezk <parvezk>
2026-05-23 19:34:47 +0530

    new quiz js

--- quiz_js_new/home/config.js
+++ quiz_js_new/home/config.js
@@ -3,7 +3,8 @@ const HomeConfig = {
         QUIZ_DATA: 'customQuizData',
     },
     URLS: {
+        BASE:         'https://sanaya-code.github.io/quiz/',
         INFO_SUBJECTS: 'https://sanaya-code.github.io/quiz/data/info/info_subjects.json',
-        QUIZ_PAGE: 'quiz/index.html',
+        QUIZ_PAGE:    '../quiz/index.html',
     },
 };
--- quiz_js_new/home/utils/remote_json_loader.js
+++ quiz_js_new/home/utils/remote_json_loader.js
@@ -1,7 +1,16 @@
-class RemoteJsonLoader {
+class RemoteJsonLoader 
+{
     async fetch(url) {
+		const resolvedUrl = this._resolveUrl(url);
         const response = await fetch(url);
         if (!response.ok) throw new Error(`HTTP ${response.status}`);
         return await response.json();
     }
+    
+    _resolveUrl(url) 
+    {
+        if (url.startsWith('http://') || url.startsWith('https://')) return url;
+        return HomeConfig.URLS.BASE + url;
+    }
+
 }
