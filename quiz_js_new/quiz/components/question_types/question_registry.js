4407cca80bb2895b50f53ea5db0c624e43671f59
parvezk <parvezk>
2026-06-01 10:14:47 +0530

    new quiz js

--- quiz_js_new/quiz/components/question_types/question_registry.js
+++ quiz_js_new/quiz/components/question_types/question_registry.js
@@ -18,14 +18,24 @@ const QuestionRegistry = {
 QuestionRegistry.register('mcq',                 'mcq-radio',          McqEvaluator);
 QuestionRegistry.register('true_false',          'true-false',         TrueFalseEvaluator);
 QuestionRegistry.register('multi_select',        'multi-select',       MultiSelectEvaluator);
+
 QuestionRegistry.register('matching_drag_drop',  'matching-select',    MatchingSelectEvaluator);
 QuestionRegistry.register('matching_select',     'matching-select',    MatchingSelectEvaluator);
+
 QuestionRegistry.register('matching_connection', 'matching-connection', MatchingConnectionEvaluator);
 QuestionRegistry.register('ordering_horizontal', 'ordering-horizontal-drag-click', OrderingHorizontalEvaluator);
 QuestionRegistry.register('fill_in_blank_operation', 'fill-in-blank-operation', FillInBlankOperationEvaluator);
 QuestionRegistry.register('compare_quantities', 'compare-quantities', CompareQuantitiesEvaluator);
 QuestionRegistry.register('matching_connection_image', 'matching-connection-image', MatchingConnectionImageEvaluator);
 QuestionRegistry.register('ordering', 'ordering-drag-drop', OrderingEvaluator);
+QuestionRegistry.register('clock_set_time', 'clock-set-time', ClockSetTimeEvaluator);
+QuestionRegistry.register('number_line_arcs', 'number-line-arcs', NumberLineArcsEvaluator);
+QuestionRegistry.register('multi_select_circle', 'multi-select-circle', MultiSelectCircleEvaluator);
+QuestionRegistry.register('multi_select_two', 'multi-select-two', MultiSelectTwoEvaluator);
+QuestionRegistry.register('short_answer', 'short-answer', ShortAnswerEvaluator);
+QuestionRegistry.register('matching',          'matching-dropdown', MatchingDropdownEvaluator);
+QuestionRegistry.register('image_compare_quantities_tick',         'compare-image-objects', CompareImageObjectsEvaluator);
+QuestionRegistry.register('fill_in_blank', 'fill-in-blank', FillInBlankEvaluator);
 
 QuestionRegistry.register('table_image_fill_in_the_blank', 'table-image-fill-in-the-blank', TableImageFillInBlankEvaluator);
 QuestionRegistry.register('table_image_fill_in_the_blank_2_col', 'table-image-fill-in-the-blank', TableImageFillInBlankEvaluator);
@@ -36,11 +46,3 @@ QuestionRegistry.register('options_fill_in_blank', 'options-fill-in-blank', Opti
 QuestionRegistry.register('fill_in_blank_multi_graph_text', 'fill-in-blank-multi-graph-text', FillInBlankMultiGraphTextEvaluator);
 QuestionRegistry.register('fill_in_blank_multi_graph', 'fill-in-blank-multi-graph-text', FillInBlankMultiGraphTextEvaluator);
 
-QuestionRegistry.register('clock_set_time', 'clock-set-time', ClockSetTimeEvaluator);
-QuestionRegistry.register('number_line_arcs', 'number-line-arcs', NumberLineArcsEvaluator);
-QuestionRegistry.register('multi_select_circle', 'multi-select-circle', MultiSelectCircleEvaluator);
-QuestionRegistry.register('multi_select_two', 'multi-select-two', MultiSelectTwoEvaluator);
-QuestionRegistry.register('short_answer', 'short-answer', ShortAnswerEvaluator);
-QuestionRegistry.register('matching',          'matching-dropdown', MatchingDropdownEvaluator);
-QuestionRegistry.register('image_compare_quantities_tick',         'compare-image-objects', CompareImageObjectsEvaluator);
-QuestionRegistry.register('fill_in_blank', 'fill-in-blank', FillInBlankEvaluator);
