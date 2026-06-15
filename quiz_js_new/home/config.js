5064faab15a43fc504b092b7cd2fd929f248d92c
parvezk <parvezk>
2026-06-15 18:25:56 +0530

    new quiz js

--- quiz_js_new/home/config.js
+++ quiz_js_new/home/config.js
@@ -3,7 +3,7 @@ const HomeConfig = {
         QUIZ_DATA: 'customQuizData',
     },
     URLS: {
-        BASE:          'https://sanaya-code.github.io/quiz/',
+        BASE:          'https://sanaya-code.github.io/quiz_js_new/',
         INFO_SUBJECTS: 'data/info/info_subjects.json',
         QUIZ_PAGE:     'quiz.html',
     },
