2839789205e7dac7cc734769f63a6eb84d19c143
parvezk <parvezk>
2026-06-26 10:02:12 +0530

    quiz josn

--- info/data_scripts/miriam.js
+++ info/data_scripts/miriam.js
@@ -3,7 +3,7 @@
 var miriam = `[
   { "label": "Quiz Local", "url": "file:///mnt/0cbf398f-1dda-49a3-b3aa-8919127e7eef/repos/sanaya-code.github.io/quiz/index.html" },
   { "label": "Quiz", "url": "https://sanaya-code.github.io/quiz_js_new/" },
-  { "label": "Class 1 drive", "url": "https://drive.google.com/drive/u/4/folders/1_AuwOi6CcabXFo-hOm6mZvdz-wa-U0mw" },
+  { "label": "Class 2 drive", "url": "https://drive.google.com/drive/u/1/folders/1en1BKCPLUEua_wwmOJvfaGVhnuPHAnVY" },
   { "label": "Miriam Drive", "url": "https://drive.google.com/drive/folders/1ZycjwMmNaHQpgNn7VKEYx2LDSaiFe5-y?usp=drive_link" },
   { "label": "School Notes", "url": "https://drive.google.com/drive/folders/1oDfgBlA1_R4gKzNsJTG7tmQN4gnG8IWx" },
   { "label": "info", "url": "https://docs.google.com/spreadsheets/d/1SXTgTBl8ls8qrfvpleTosNAxNx944R5rZsmEsaC89Z8/edit?gid=0#gid=0" },
