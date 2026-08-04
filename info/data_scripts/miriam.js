199a47b7015abfefe83269dc06f3a87fc2d9c0bb
parvezk <parvezk>
2026-08-04 09:36:33 +0530

    info

--- info/data_scripts/miriam.js
+++ info/data_scripts/miriam.js
@@ -7,6 +7,9 @@ var miriam = `[
   { "label": "Miriam Drive", "url": "https://drive.google.com/drive/folders/1ZycjwMmNaHQpgNn7VKEYx2LDSaiFe5-y?usp=drive_link" },
   { "label": "School Notes", "url": "https://drive.google.com/drive/folders/1oDfgBlA1_R4gKzNsJTG7tmQN4gnG8IWx" },
   { "label": "info", "url": "https://docs.google.com/spreadsheets/d/1SXTgTBl8ls8qrfvpleTosNAxNx944R5rZsmEsaC89Z8/edit?gid=0#gid=0" },
-  { "label": "fee", "url": "https://sttheresaerp.com/DefaultMIS1.aspx" }
+  { "label": "fee", "url": "https://stxavierbpl.schoolerps.com/DefaultMIS1.aspx" },
+  {"label": "School Xaviers", "url":"https://www.stxaviersbhopal.org/"},
+  {"label": "Test", "url":"test"}
+    
   ]`;
   
--- info/data_scripts/sanaya.js
+++ info/data_scripts/sanaya.js
@@ -4,8 +4,9 @@
     { "label": "Class-7 Drive", "url": "https://drive.google.com/drive/u/1/folders/17cmZ5Y_TT9ixze05QGAEplqJJe2dbAN5" },
     {"label": "Github sanaya", "url":"https://github.com/sanaya-code/sanaya-code.github.io/tree/main/quiz/data/school/c1_miriam"}, 
     {"label": "Github Actions", "url":"https://github.com/sanaya-code/sanaya-code.github.io/actions"},
-    {"label": "Sanaya Fee", "url":"https://stxbplerp.in/DefaultMIS1.aspx"}, 
+    {"label": "Sanaya Fee", "url":"https://stxavierbpl.schoolerps.com/DefaultMIS1.aspx"}, 
     { "label": "Quiz Local", "url": "file:///mnt/0cbf398f-1dda-49a3-b3aa-8919127e7eef/repos/sanaya-code.github.io/quiz/index.html" },
     { "label": "Quiz", "url": "https://sanaya-code.github.io/quiz_js_new/" },
+    {"label": "School Xaviers", "url":"https://www.stxaviersbhopal.org/"},
     {"label": "Test", "url":"test"}
   ]`;
