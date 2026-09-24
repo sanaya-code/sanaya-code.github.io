d6ecea66f6ff6a3efa05b5e8ce5a0b9bb1e0ac02
parvezk <parvezk>
2026-09-24 23:40:18 +0530

    info

--- info/data_scripts/plot.js
+++ info/data_scripts/plot.js
@@ -16,6 +16,7 @@ var plot = `[
   { "label": "Electricipy bill", "url":"https://resourceutils.mpcz.in:8090/payBill"},
   { "label": "Bhopal nagar nigam", "url":"https://www.bmconline.gov.in/irj/portal/anonymous/ProRelServices"},
   { "label": "rajendra_kushwah_9893266322_N2411003996_Ph00093939", "url":""},
+  { "label": "gorelal_kushwah_7389131087_N2411016220", "url":""},
   { "label": "tulsi_kushwah_Ph00804509", "url":""},
   { "label": "Test", "url":"test"}
 ]`;
